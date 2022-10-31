81e0614216fc2178737a6823b1e2ce6d376e945f
Benoît Daene <benoitdaene@gmail.com>
2022-10-31 23:58:46 +0100

    Factorize definitions out of main.

--- main.py
+++ main.py
@@ -18,6 +18,14 @@ def get_corner(group_id, colors, transform):
     ])
 
 
+def get_definitions(colors):
+    for rotation in range(0, 360, 90):
+        yield get_straight(f'straight_out_{rotation:03}', colors, [svg.Rotate(rotation)])
+        yield get_straight(f'straight_in_{rotation:03}', colors[::-1], [svg.Rotate(rotation)])
+        yield get_corner(f'corner_out_{rotation:03}', colors, [svg.Rotate(rotation)])
+        yield get_corner(f'corner_in_{rotation:03}', colors[::-1], [svg.Rotate(rotation)])
+
+
 def draw(size, out):
     offset = (size - 1) / 2 * 4
     if out:
@@ -44,30 +52,23 @@ def main(max_size, colors=True):
         colors = ('#dd7777', '#FFB60C')
     else:
         colors = ('#00000000', '#ffffff00')
+
     scale = 20
     canvas_size = 4 * (max_size + 1) * scale
     offset = canvas_size // 2
 
-    definitions = svg.Defs(elements=[])
-    for rotation in range(0, 360, 90):
-        definitions.elements.extend([
-            get_straight(f'straight_out_{rotation:03}', colors, [svg.Rotate(rotation)]),
-            get_straight(f'straight_in_{rotation:03}', colors[::-1], [svg.Rotate(rotation)]),
-            get_corner(f'corner_out_{rotation:03}', colors, [svg.Rotate(rotation)]),
-            get_corner(f'corner_in_{rotation:03}', colors[::-1], [svg.Rotate(rotation)]),
-        ])
-
+    definitions = svg.Defs(elements=list(get_definitions(colors)))
     main_group = svg.G(elements=[], transform=[svg.Translate(offset, offset), svg.Scale(scale)])
     canvas = svg.SVG(width=canvas_size, height=canvas_size, elements=[definitions, main_group])
 
     out = True
     for size in range(2 + max_size % 2, max_size + 1, 2):
         main_group.elements.extend(draw(size, out))
-        out ^= True
+        out = not out
 
     with open('cool-s.svg', 'w') as file:
         print(canvas, file=file)
 
 
 if __name__ == "__main__":
-    main(max_size=5, colors=False)
+    main(max_size=6, colors=True)
